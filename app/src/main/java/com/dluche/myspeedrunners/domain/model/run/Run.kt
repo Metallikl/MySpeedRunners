f1004e5399d19f193e14ca7ceed9620471e83bf0
DanielLuche <daniel.betteloni@gmail.com>
2025-03-28 13:22:49 -0300

    WIP  Creating domain models  Create RunDataSource.kt and repoitory

--- app/src/main/java/com/dluche/myspeedrunners/domain/model/run/Run.kt
+++ app/src/main/java/com/dluche/myspeedrunners/domain/model/run/Run.kt
@@ -1,11 +1,8 @@
 package com.dluche.myspeedrunners.domain.model.run
 
-import com.dluche.myspeedrunners.data.datasource.model.common.LinkDto
 import com.dluche.myspeedrunners.data.datasource.model.run.PlayerDto
-import com.dluche.myspeedrunners.data.datasource.model.run.StatusDto
 import com.dluche.myspeedrunners.data.datasource.model.run.SystemDto
 import com.dluche.myspeedrunners.data.datasource.model.run.TimesDto
-import com.dluche.myspeedrunners.data.datasource.model.run.VideosDto
 import com.dluche.myspeedrunners.domain.model.common.LinkModel
 
 data class Run(
