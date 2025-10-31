79c65d34c3ecb90e1dd001ce8aab84d45e6e8f5e
DanielLuche <daniel.betteloni@gmail.com>
2025-10-31 17:18:22 -0300

    Refactor: Paginate runner runs
    - Add `SearchRunnerRunsUseCase` to fetch paginated runs for a specific runner.
    - Implement `Pager` in `RunsRepositoryImpl` to provide a `Flow<PagingData<Run>>`.
    - Update `RunsRepository` interface with the new `searchRunnerRuns` method.

--- app/src/main/java/com/dluche/myspeedrunners/data/repository/RunsRepositoryImpl.kt
+++ app/src/main/java/com/dluche/myspeedrunners/data/repository/RunsRepositoryImpl.kt
@@ -1,15 +1,23 @@
 package com.dluche.myspeedrunners.data.repository
 
 import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.dluche.myspeedrunners.data.IoDispatcher
 import com.dluche.myspeedrunners.data.datasource.run.RunDataSource
 import com.dluche.myspeedrunners.data.mapper.asDomainModel
+import com.dluche.myspeedrunners.data.paging.RunnerRunsPagingSource
+import com.dluche.myspeedrunners.data.paging.RunnersPagingSource
+import com.dluche.myspeedrunners.data.repository.RunnersRepositoryImpl.Companion.PAGE_SIZE
 import com.dluche.myspeedrunners.domain.model.common.EmbedParams
 import com.dluche.myspeedrunners.domain.model.common.QueryOrderBy
 import com.dluche.myspeedrunners.domain.model.run.PaginatedRun
 import com.dluche.myspeedrunners.domain.model.run.Run
 import com.dluche.myspeedrunners.domain.repository.RunsRepository
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -29,6 +37,29 @@ class RunsRepositoryImpl @Inject constructor(
                 ).asDomainModel()
             }
         }
+    }
+
+    override suspend fun searchRunnerRuns(
+        runnerId: String,
+        embedParams: EmbedParams?,
+        queryOrderBy: QueryOrderBy?
+    ): Flow<PagingData<Run>> {
+        return Pager(
+            PagingConfig(
+                pageSize = PAGE_SIZE,
+                initialLoadSize = PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                RunnerRunsPagingSource(
+                    runDataSource = dataSource,
+                    runnerId = runnerId,
+                    embedParams = embedParams,
+                    queryOrderBy = queryOrderBy,
+                )
+            }
+        ).flow.flowOn(dispatcher)
+
     }
 
     override suspend fun getRuns(embedParams: EmbedParams?): Result<PaginatedRun> {
--- app/src/main/java/com/dluche/myspeedrunners/domain/repository/RunsRepository.kt
+++ app/src/main/java/com/dluche/myspeedrunners/domain/repository/RunsRepository.kt
@@ -1,13 +1,18 @@
 package com.dluche.myspeedrunners.domain.repository
 
+import androidx.paging.PagingData
+import com.dluche.myspeedrunners.data.datasource.model.run.RunWrapperDto
 import com.dluche.myspeedrunners.domain.model.common.EmbedParams
 import com.dluche.myspeedrunners.domain.model.common.QueryOrderBy
 import com.dluche.myspeedrunners.domain.model.run.PaginatedRun
 import com.dluche.myspeedrunners.domain.model.run.Run
+import kotlinx.coroutines.flow.Flow
 
 interface RunsRepository {
 
     suspend fun getRunnerRuns(runnerId: String,embedParams: EmbedParams?, queryOrderBy: QueryOrderBy?): Result<PaginatedRun>
+
+    suspend fun searchRunnerRuns(runnerId: String,embedParams: EmbedParams?, queryOrderBy: QueryOrderBy?): Flow<PagingData<Run>>
 
     suspend fun getRuns(embedParams: EmbedParams?): Result<PaginatedRun>
 
