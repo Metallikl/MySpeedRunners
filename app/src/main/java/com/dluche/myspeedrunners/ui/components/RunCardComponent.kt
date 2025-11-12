e518e6c52a39f7cab856859fb46c5ea8fb6539f1
DanielLuche <daniel.betteloni@gmail.com>
2025-11-12 17:10:30 -0300

    Refactor: Improve RunCardComponent layout
    - Adjust RunCardComponent to handle long game titles with multiple lines.
    - Set a max height for the card's row and ensure the image fills the height.
    - Add `.idea/deploymentTargetSelector.xml` to gitignore.

--- app/src/main/java/com/dluche/myspeedrunners/ui/components/RunCardComponent.kt
+++ app/src/main/java/com/dluche/myspeedrunners/ui/components/RunCardComponent.kt
@@ -6,10 +6,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -41,25 +48,25 @@ fun RunCard(
     contentDescription: String? = null,
     onClick: () -> Unit
 ) {
-
     Card(
         modifier = modifier
             .fillMaxWidth()
             .clickable {
                 onClick()
             }
-
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .heightIn(max = 118.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
 
             RunImage(
                 gameUrl = gameUrl,
                 contentDescription = contentDescription,
-                size = 100.dp
+                size = 100.dp,
             )
 
             Column(
@@ -69,8 +76,12 @@ fun RunCard(
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
                     text = gameName,
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
                     text = category,
@@ -132,8 +143,9 @@ fun RunImage(gameUrl: String, contentDescription: String?, size: Dp) {
                 painter = painter,
                 contentDescription = contentDescription,
                 modifier = Modifier
-                    .size(size),
-                contentScale = ContentScale.FillBounds
+                    .width(size)
+                    .fillMaxHeight(),
+                contentScale = ContentScale.FillBounds,
             )
         }
     }
